9e3c1c51c5ce9f895fadec1e9f97cc8065638878
Yogesh agarwal <yogesh@Yogeshs-MacBook-Pro.local>
2016-11-29 21:22:27 +0530

    Add foundation and jquery

--- public/app.jsx
+++ public/app.jsx
@@ -7,6 +7,11 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+$(document).foundation();
+
 ReactDOM.render(
     <Router history = {hasHistory}>
       <Route path = "/" component={Main}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './public/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './public/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery' : 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundel.js'
