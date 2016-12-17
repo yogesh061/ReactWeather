f7ce476a8ff94a8a70677837d81883c6ca9fb1af
Yogesh agarwal <yogesh@Yogeshs-MacBook-Pro.local>
2016-12-17 22:39:26 +0530

    Updated About you page

--- public/components/About.jsx
+++ public/components/About.jsx
@@ -11,8 +11,19 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-        <h3>About</h3>
-        <p>Welcome to about page</p>
+        <h1 className="text-center">About</h1>
+        <p>This is a Weather application build on React JS.</p>
+        <p>
+          Here are some of the tools I used:
+        </p>
+        <ol>
+          <li>
+            <a href="https://facebook.github.io/react/">React</a>- This was the javascript framework used.
+          </li>
+          <li>
+            <a href="https://openweathermap.org/">Open Weather Map</a>- Open source weather map.
+          </li>
+        </ol>
     </div>
 
   );
