d97bf6d1a0f1652e3912aa087f276b0c24ca53da
Yogesh agarwal <yogesh@Yogeshs-MacBook-Pro.local>
2016-12-24 18:42:46 +0530

    Linked Search Weather Field

--- public/components/Nav.jsx
+++ public/components/Nav.jsx
@@ -16,7 +16,12 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch:function(e){
     e.preventDefault();
-    alert("TBD");
+    var location  = this.refs.search.value;
+    var encodedLocation = encodeURIComponent(location);
+    if(location.length > 0){
+      this.refs.search.value  = '';
+      window.location.hash  = '#/?location='+encodedLocation;
+    }
   },
   render:function(){
     return(
@@ -39,7 +44,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search Weather"/>
+                <input type="search" placeholder="Search Weather" ref="search"/>
               </li>
               <li>
                 <input type="submit" className="button" value = "Get Weather"/>
--- public/components/Weather.jsx
+++ public/components/Weather.jsx
@@ -16,7 +16,9 @@ var Weather = React.createClass({
     debugger;
     this.setState({
       isLoading:true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
     openWeatherMap.getTemp(location).then(function(temp){
         that.setState({
@@ -33,6 +35,20 @@ var Weather = React.createClass({
         });
     });
 
+  },
+  componentDidMount:  function(){
+    var location  = this.props.location.query.location;
+    if(location && location.length > 0){
+      this.handelSearch(location);
+      window.location.hash  = '#/';
+    }
+  },
+  componentWillReceiveProps:  function(newProps){
+    var location  = newProps.location.query.location;
+    if(location && location.length > 0){
+      this.handelSearch(location);
+      window.location.hash  = '#/';
+    }
   },
   render: function(){
     var {isLoading, temp, location, errorMessage} = this.state;
